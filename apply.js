d3f8307081340f00eba3fe580abe644b2fb52816
Laura Miller <mslrmiller@gmail.comm>
2018-03-25 13:04:09 -0600

    create initial variables and rename buttons.

---
+++ apply.js
@@ -0,0 +1,27 @@
+var userTitle = document.querySelector('.website-title').required;
+var userUrl = document.querySelector('.website-url').required;
+var enterUserInput= document.querySelector('.enter-button');
+var readButton = document.querySelector('#read-button');
+var deleteButton = document.querySelector('#delete-button');
+
+
+
+
+enterUserInput.addEventListener('click', captureUserInput);
+
+
+
+function captureUserInput() {
+
+  
+}
+
+
+
+
+
+
+
+
+
+
