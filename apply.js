35e110b2d39cf091c3057dc449d9edffdd1a87c6
Laura Miller <mslrmiller@gmail.comm>
2018-03-28 16:52:30 -0600

    url confirmaiton and validation test and prevent default.

--- apply.js
+++ apply.js
@@ -7,7 +7,7 @@ var bookMarkList = document.querySelector('#listed-bookmarks');
 
 
 
-enterUserInput.addEventListener('click', captureUserInput);
+enterUserInput.addEventListener('click', urlval);
 
 $('#listed-bookmarks').on('click', '#read-button', readButtonClicked);
 $('#listed-bookmarks').on('click', '#delete-button', deleteButtonClicked);
@@ -19,14 +19,13 @@ function readButtonClicked() {
   $(this).parent().toggleClass('ulClicked');
 }
 
-function deleteButtonClicked() {
+function deleteButtonClicked () {
   console.log('This damn delete button');
     $(this).parent().remove();
 }
 
 function captureUserInput() {
   var newBookmark = document.createElement ('ul');
-
   newBookmark.innerHTML = 
     `<h2>${userTitle.value}</h2>
      <hr>
@@ -35,26 +34,25 @@ function captureUserInput() {
      <button id = "read-button">Read</button>
      <button id = "delete-button">Delete</button>
     `;
-  bookMarkList.prepend(newBookmark);
-  clearInput();
+    bookMarkList.prepend(newBookmark);
+    clearInput();
 }
+
+function urlval() {
+    var testedUrl = userUrl.value;
+    var linkRegex =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    if(linkRegex.test(testedUrl) === true) {
+       captureUserInput();
+    } else {
+       alert ('please enter a valid URL');
+    }
+}
+
 
 function clearInput() {
   userTitle.value= "";
   userUrl.value= "";
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
